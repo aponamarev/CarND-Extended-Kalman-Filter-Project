dc99aa252aa5b3b5e39402f5a0ee597210e1c0a5
aponamarev-arajoy <alexander.ponamarev@arajoy.ai>
2017-10-21 20:48:39 -0400

    Finished RMSE.

--- src/tools.cpp
+++ src/tools.cpp
@@ -12,9 +12,35 @@ Tools::~Tools() {}
 VectorXd Tools::CalculateRMSE(const vector<VectorXd> &estimations,
                               const vector<VectorXd> &ground_truth) {
   /**
-  TODO:
-    * Calculate the RMSE here.
+  Returns RMSE
   */
+    VectorXd RMSE(4);
+    RMSE << 0,0,0,0;
+    
+    if (estimations.size()>0) {
+        if (estimations.size()==ground_truth.size()) {
+            
+            for (unsigned int i=0; i<estimations.size(); i++) {
+                
+                VectorXd d = estimations[i] - ground_truth[i];
+                d = d.array() * d.array();
+                d = d.array().sqrt();
+                cout << "RMSE: " << d.transpose() << endl;
+                RMSE += d;
+                
+            }
+            
+            RMSE /= estimations.size();
+            
+        } else {
+            cout << "Error: Incompatible input size. Estimations size is 0 and it should be greater." << endl;
+        }
+    }
+    else {
+        cout << "Error: Incorrect input size. Estimations size is 0 and it should be greater." << endl;
+    }
+    
+    return RMSE;
 }
 
 MatrixXd Tools::CalculateJacobian(const VectorXd& x_state) {
